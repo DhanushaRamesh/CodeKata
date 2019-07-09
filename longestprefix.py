8a450554f40900fabe9bc72b6e2fa6cee3d046d6
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-07-09 16:13:40 +0530

    Create longestprefix.py
    find longest prefix in a given range of strings

---
+++ longestprefix.py
@@ -0,0 +1,29 @@
+lstit=[]
+class longest:
+	def prefix(self,str1,str2):
+		n=0
+		lstit=[]
+		if len(str1)<len(str2):
+			n=len(str1)
+		else:
+			n=len(str2)
+		for i in range(n):
+			if str1[i]==str2[i]:
+				lstit.append(str1[i])
+		return(lstit)
+		
+		
+n=int(input())
+lst=[]
+for i in range(n):
+	stri=input()
+	lst.append(stri)
+	
+call=longest()
+for i in range(1,n):
+	longetprefix=call.prefix(lst[0],lst[i])
+for i in range(len(longetprefix)):
+	if i == len(longetprefix)-1:
+		print(longetprefix[i])
+	else:
+		print(longetprefix[i],end="")
